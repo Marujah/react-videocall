b5b3465d7cc6509fd64bceae74171960856af118
Marouen Mhiri <marujah@gmail.com>
2020-04-11 12:30:59 +0200

    add db connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const https = require('https');
 const express = require('express');
 const enforce = require('express-sslify');
 const fs = require('fs');
+const { Client } = require('pg');
 const config = require('../config');
 const socket = require('./lib/socket');
 
@@ -11,9 +12,32 @@ const cert = fs.readFileSync('./cert.pem');
 const app = express();
 const server = https.createServer({ key, cert }, app);
 
+
+const client = new Client({
+  connectionString: config.DB.uri,
+  ssl: true
+});
+
+client.connect();
+
+const getUsers = (request, response) => {
+  client.query(`CREATE TABLE IF NOT EXISTS users (
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    email VARCHAR(30)
+  )`, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
 app.use(enforce.HTTPS());
 
 app.use('/', express.static(`${__dirname}/../client`));
+
+app.use('/users', getUsers);
 
 server.listen(config.PORT, () => {
   socket(server);
